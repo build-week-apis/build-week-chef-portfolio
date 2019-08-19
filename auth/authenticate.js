d188e50db89b441c469b70ed88952f28b83d6f0c
Donald Whitely <dswhitely1@gmail.com>
2019-08-18 21:39:40 -0400

    Setup PG for Heroku, testing locally

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,40 +1,40 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const jwtKey = process.env.JWT_SECRET;
+const jwtKey = process.env.JWT_SECRET || 'testing';
 
 function authenticate(req, res, next) {
-  const token = req.get('Authorization');
+	const token = req.get('Authorization');
 
-  if (token) {
-    jwt.verify(token, jwtKey, (err, decoded) => {
-      if (err) return res.status(401).json(err);
+	if (token) {
+		jwt.verify(token, jwtKey, (err, decoded) => {
+			if (err) return res.status(401).json(err);
 
-      req.decoded = decoded;
+			req.decoded = decoded;
 
-      next();
-    });
-  } else {
-    return res.status(401).json({
-      error: 'No token provided, must be set on the Authorization Header',
-    });
-  }
+			next();
+		});
+	} else {
+		return res.status(401).json({
+			error : 'No token provided, must be set on the Authorization Header',
+		});
+	}
 }
 
 function generateToken(user) {
-  const payload = {
-    sub: user.id,
-    username: user.username,
-  };
+	const payload = {
+		sub      : user.id,
+		username : user.username,
+	};
 
-  const options = {
-    expiresIn: '7d',
-  };
+	const options = {
+		expiresIn : '7d',
+	};
 
-  return jwt.sign(payload, jwtKey, options);
+	return jwt.sign(payload, jwtKey, options);
 }
 
 module.exports = {
-  authenticate,
-  generateToken,
+	authenticate,
+	generateToken,
 };
--- knexfile.js
+++ knexfile.js
@@ -3,59 +3,63 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/database.db3',
-    },
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-    useNullAsDefault: true,
-  },
+	development : {
+		client           : 'sqlite3',
+		connection       : {
+			filename : './data/database.db3',
+		},
+		migrations       : {
+			directory : './data/migrations',
+		},
+		seeds            : {
+			directory : './data/seeds',
+		},
+		useNullAsDefault : true,
+	},
 
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/testing.db3',
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
+	testing     : {
+		client           : 'sqlite3',
+		connection       : {
+			filename : './data/testing.db3',
+		},
+		useNullAsDefault : true,
+		migrations       : {
+			directory : './data/migrations',
+		},
+		seeds            : {
+			directory : './data/seeds',
+		},
+	},
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
+	staging     : {
+		client     : 'postgresql',
+		connection : {
+			database : 'my_db',
+			user     : 'username',
+			password : 'password',
+		},
+		pool       : {
+			min : 2,
+			max : 10,
+		},
+		migrations : {
+			tableName : 'knex_migrations',
+		},
+	},
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
+	production  : {
+		client     : 'pg',
+		connection : process.env.DATABASE_URL,
+		pool       : {
+			min : 2,
+			max : 10,
+		},
+		migrations : {
+			tableName : 'knex_migrations',
+			directory : './data/migrations',
+		},
+		seeds      : {
+			directory : './data/seeds',
+		},
+	},
 };
